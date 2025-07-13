0b1466fd05001ea7fc9e6cd2b28cb2be015c9a45
Oleksiy Beseda <133978223+Oleksiy-Beseda@users.noreply.github.com>
2025-07-14 02:21:20 +0300

    My third commit AQA Python

--- pages/form_page.py
+++ pages/form_page.py
@@ -1,7 +1,25 @@
+import time
+
 from pages.base_page import BasePage
+from locators.form_page_locators import FormPageLocators as Locators
+
+
 
 
 class FormPage(BasePage):
 
     def fill_fields_and_submit(self):
-        pass
+        first_name = 'Hello'
+        last_name = 'World'
+        email = "hello@world.com"
+        self.element_is_visible(Locators.FIRST_NAME).send_keys(first_name)
+        self.element_is_visible(Locators.LAST_NAME).send_keys(last_name)
+        self.element_is_visible(Locators.EMAIL).send_keys(email)
+        self.element_is_visible(Locators.GENDER).click()
+        self.element_is_visible(Locators.MOBILE).send_keys('5423423423')
+        self.element_is_visible(Locators.SUBJECT).send_keys('English')
+        self.element_is_visible(Locators.HOBBIES).click()
+        self.element_is_visible(Locators.FILE_INPUT).send_keys(r'/Users/oleksiybeseda/PycharmProjects/PythonAQATestProject/.venv311/test.txt')
+        self.element_is_visible(Locators.CURRENT_ADDRESS).send_keys('City, 1231, USA')
+        self.element_is_visible(Locators.SUBMIT).click()
+        time.sleep(5)
--- tests/form_test.py
+++ tests/form_test.py
@@ -6,3 +6,4 @@ class TestFormPage:
     def test_form(self, driver):
         form_page = FormPage(driver, 'https://demoqa.com/automation-practice-form')
         form_page.open()
+        form_page.fill_fields_and_submit()
