d57a92f96af0828c3d847632adf7626a1452a0a4
Oleksiy Beseda <beseda.odessa@gmail.com>
2025-11-11 00:29:46 +0200

    Update simple_button.py

--- pages/simple_button.py
+++ pages/simple_button.py
@@ -1,6 +1,16 @@
 from pages.base_page import BasePage
+from selenium.webdriwer.common.by import By
 
+
+button_selector = (By.ID, 'submit-id-submit')
+result_selector = (By.ID, 'result-text')
 
 class SimpleButtonPage(BasePage):
     def __init__(self, browser):
         super().__init__(browser)
+
+    def button(self):
+        return self.find(button_selector)
+
+    def result(self):
+        return self.find(result_selector)
